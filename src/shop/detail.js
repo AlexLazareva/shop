234bca7344b30c31ab1ee17056e52f037200a4be
Alex Lazareva <alex.lazareva25@yandex.ru>
2017-07-10 13:08:57 +0300

    create the html markup Detail component

--- src/shop/detail.js
+++ src/shop/detail.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 
 export default class Detail extends React.Component {
 
-    render() {
-        return (
-            <div>
+    static propTypes = {
+        phone: PropTypes.string.isRequired,
+        className: PropTypes.string.isRequired
+    };
 
+    render() {
+        const {phone, className} = this.props;
+
+        return (
+            <div className={ className }>
+                <h3>{ phone.name }</h3>
+                <small>{ phone.id }</small>
+                <img src={ phone.imgUrl } alt={ phone.name } />
+                <p>{ phone.snippet }</p>
             </div>
         );
     }
