dd973a78aa7007f74ea8a663ca2359885a6471f6
Alex Lazareva <alex.lazareva25@yandex.ru>
2017-07-10 12:46:58 +0300

    add constructor to shop and handleClick

---
+++ src/shop/detail.js
@@ -0,0 +1,3 @@
+/**
+ * Created by Alex_Lazareva on 10.07.2017.
+ */
--- src/shop/index.js
+++ src/shop/index.js
@@ -1,16 +1,29 @@
 import React from 'react';
 import phones from './phones.json';
+import Detail from './detail';
 import './styles.less';
 
 export default class Shop extends React.Component {
 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            phone: phones[0]
+        };
+    }
+
     mapPhones(phone, i) {
         return (
-            <li key={i}>
+            <li key={i} onClick={this.handleClick.bind(this.phone)}>
                 { phone.name }
                 <img src={phone.imageUrl} />
             </li>
         );
+    }
+
+    handleClick(phone) {
+        this.setState({ phone });
     }
 
     render() {
